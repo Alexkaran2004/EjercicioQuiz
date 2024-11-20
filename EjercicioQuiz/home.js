74e677e44bdbe451f1f44d9705ede1b44fd7b962
Alexkaran2004 <karandeniyalex@gmail.com>
2024-11-20 17:03:25 +0100

    quiz

--- EjercicioQuiz/home.js
+++ EjercicioQuiz/home.js
@@ -1,33 +1,3 @@
-// function startQuiz() {
-//     window.location.href = 'questions.html';
-// }
-
-// function loadResults() {
-//     let results = JSON.parse(localStorage.getItem('quizResults')) || [];
-//     let resultsList = document.getElementById('resultsList');
-//     let ctx = document.getElementById('resultsChart').getContext('2d');
-//     let labels = results.map (results => results.date);
-//     let data = results.map (results => results.score);
-//     resultsList.innerHTML = '';
-//     results.forEach(result => {
-//         let div = document.createElement('div');
-//         div.innerText = `Fecha: ${result.date} - Puntuación: ${result.score}`;
-//         resultsList.appendChild(div);
-//     });
-//     new Chart(ctx, { 
-//         type: 'bar', data: { 
-//         labels: labels,
-//          datasets: [{ 
-//             label: 'Puntuación', 
-//             data: data, 
-//             borderColor: 'rgba(75, 192, 192, 1)', borderWidth: 1 
-
-//         }]
-//     }
-// })
-// }
-
-    
 function startQuiz() {
     window.location.href = 'questions.html';
 }
--- EjercicioQuiz/trivial.js
+++ EjercicioQuiz/trivial.js
@@ -1,23 +1,23 @@
-fetch('https://opentdb.com/api.php?amount=10&category=27&difficulty=hard')
-.then(res => res.json())
-.then(json => console.log(json));
+// fetch('https://opentdb.com/api.php?amount=10&category=27&difficulty=hard')
+// .then(res => res.json())
+// .then(json => console.log(json));
 
-function questions() {
-let preguntas = document.createElement('div');
-let contenidopreguntas = document.createElement('');
-let textopreguntas = document.createTextNode(text);
-preguntas.appendChild(textopreguntas);
-contenidopreguntas.appendChild(preguntas);
-document.body.appendChild(contenidopreguntas);
+// function questions() {
+// let preguntas = document.createElement('div');
+// let contenidopreguntas = document.createElement('');
+// let textopreguntas = document.createTextNode(text);
+// preguntas.appendChild(textopreguntas);
+// contenidopreguntas.appendChild(preguntas);
+// document.body.appendChild(contenidopreguntas);
 
-}
+// }
 
-function answers() {
-    let respuestas = document.createElement('div');
-    let contenidorespuestas = document.createElement('');
-    let textorespuestas = document.createTextNode(text); 
-    respuestas.appendChild(textorespuestas);
-    contenidorespuestas.appendChild(respuestas);
-    document.body.appendChild(contenidorespuestas);
+// function answers() {
+//     let respuestas = document.createElement('div');
+//     let contenidorespuestas = document.createElement('');
+//     let textorespuestas = document.createTextNode(text); 
+//     respuestas.appendChild(textorespuestas);
+//     contenidorespuestas.appendChild(respuestas);
+//     document.body.appendChild(contenidorespuestas);
 
-}
+// }
